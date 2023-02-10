5bc2ad37496ed660128f99d4fd142d89f0f54f9e
MANIKANTA325 <edupugantimanikanta143@gmail.com>
2023-02-10 10:34:09 +0530

    set

---
+++ datatypes.4.py
@@ -0,0 +1,8 @@
+# Set -> unordered,unindexed collection of datatypes ,{},doesn't contain a duplicate value
+
+myset={"mani","pavan",15,235.3,True,17,14}
+print(myset)
+
+# add
+myset.add("narsi")
+print(myset)
