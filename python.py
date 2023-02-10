f61523d5883bf3dfa9e7fac9f1cfa3e334d531e5
MANIKANTA325 <edupugantimanikanta143@gmail.com>
2023-02-10 10:22:57 +0530

    hii

---
+++ python.py
@@ -0,0 +1,39 @@
+# List -> collection od data, ordered ,its mutable ,[]
+mylist=["mani","pavan","when","naresh",255,False,325.63]
+print("The value of mylist is",mylist)
+print(type(mylist))
+# Index value -> prints individual data
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+print(mylist[3])
+print(mylist[4])
+print(mylist[5])
+print(mylist[6])
+
+# changeable
+# Update
+mylist[-2]=True
+print("The updates values of list is",mylist)
+
+# append(add)
+mylist.append("data",)
+print("The values after adding are",mylist)
+
+# insert
+mylist.insert(3,"hello")
+print("The value after indexing is",mylist)
+
+list1=[5,32,123]
+
+# extend
+mylist.extend(list1)
+# list1.extend(mylist)
+print("The value of list is",mylist)
+
+# Remove
+mylist.remove(255)
+print("The values are",mylist)
+
+
+
